c18f8e3186c3552574a0133d4724e390d95c6652
rodolfoizidoro <kethupe@hotmail.com>
2016-11-25 15:07:33 -0200

    rede
    rede

---
+++ app/src/main/java/makers/com/redecolabora/Login.java
@@ -0,0 +1,15 @@
+package makers.com.redecolabora;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Remove title bar
+
+        setContentView(R.layout.activity_login);
+    }
+}
